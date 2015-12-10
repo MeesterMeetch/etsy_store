172819d9406a48e58c2e9cf6608b258df7a2ae6e
MeesterMeetch <mwhunt3@gmail.com>
2015-12-10 16:10:18 -0500

    make it better

---
+++ server.js
@@ -0,0 +1,22 @@
+var express = require('express');
+var port = process.env.PORT || 3001;
+var app = express();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+var mongoose = require('mongoose');
+var mongoConfig = process.env.MONGOLAB_URI || process.env.MONGO_URI || 'mongodb://localhost:27017/test';
+var Message = require('./Message');
+
+
+app.use(express.static(__dirname + "/app"))
+
+app.get('/', function(req,response) {
+  response.sendFile('app');
+});
+
+app.get('/maybe', function(req,res) {
+  res.send("<p>what am i doing</p>");
+});
+
+
+http.listen(port);
